0907782cbf0ce23a3ed06455ec692959d28e290e
Mateus-Isaque <mateusisaque2007@gmail.com>
2026-09-03 20:30:54 -0300

    feat: Exemplo de utilizção do useState.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Cabecalho from './components/Cabecalho'
 import Conteudo from './components/Conteudo'
 import Rodape from './components/Rodape'
+import Lampada from './components/Lampada'
 
 export default function App() {
   return (
     <div>
       <Cabecalho />
+      <Lampada />
       <Conteudo />
       <Rodape />
     </div>
