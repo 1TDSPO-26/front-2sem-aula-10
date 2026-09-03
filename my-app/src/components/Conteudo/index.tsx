28a50c88a0ec390d1d2bd1096b385334fc421e37
arthurmartinss <arthurmartinsh6@gmail.com>
2026-09-03 20:02:05 -0300

    feat: Exemplo de useState

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import imgQuadrado from "../../img/quadrado.png";
 
-
 export default function Conteudo() {
+  let comum:number = 0;
+ 
+  function incrementaComum(){
+    comum = comum + 1;
+    console.log("Valor de comum:",comum);
+  }
+ 
+  const[estado,setEstado] = useState<number>(0);
+ 
+  function incrementaEstado(){
+   
+    setEstado((anterior)=> anterior + 1 );
+    console.log("Valor do estado:",estado);
+  }
+
+  document.title = "Valor do Estado : " + estado.toString();
+
   return (
     <main>
+      <div>
+        <button onClick={incrementaComum}>Aumenta Comum - {comum}</button>
+      </div>
+      <div>
+        <button onClick={incrementaEstado}>Aumenta Estado - {estado}</button>
+      </div>
       <section>
         <h1>Conteudo Aleatorio</h1>
         <p>
@@ -13,14 +36,15 @@ export default function Conteudo() {
           enim, veniam vel eveniet facere officia inventore dicta quis. Sint
           saepe a nostrum repellendus illo est inventore eos quo nulla. Ab quas
           neque cumque facilis in. Sequi ratione iure dolore cum eos at quod
-         
         </p>
       </section>
       <section>
         <h2>Imagem com referencia externa</h2>
         <figure>
-          <img src="https://placehold.co/600x400/FF0000/FFFFFF/png"
-            alt="Imagem ilustrativa do conteúdo"/>
+          <img
+            src="https://placehold.co/600x400/FF0000/FFFFFF/png"
+            alt="Imagem ilustrativa do conteúdo"
+          />
 
           <figcaption>
             Imagem utilizada para representar o conteúdo da aplicação.
@@ -30,8 +54,7 @@ export default function Conteudo() {
       <section>
         <h2>Imagem com referencia interna</h2>
         <figure>
-          <img src={imgQuadrado}
-            alt="Imagem ilustrativa do conteúdo"/>
+          <img src={imgQuadrado} alt="Imagem ilustrativa do conteúdo" />
 
           <figcaption>
             Imagem utilizada para representar o conteúdo da aplicação.
@@ -41,16 +64,13 @@ export default function Conteudo() {
       <section>
         <h2>Imagem com referencia estatica</h2>
         <figure>
-          <img src="/quadrado-2.png"
-            alt="Imagem ilustrativa do conteúdo"/>
+          <img src="/quadrado-2.png" alt="Imagem ilustrativa do conteúdo" />
 
           <figcaption>
             Imagem utilizada para representar o conteúdo da aplicação.
           </figcaption>
         </figure>
       </section>
-      
-
     </main>
   );
 }
