ba20792eee4338f51f3f07ad561e467188f56b5b
melissafiuza <melfiuza153@gmail.com>
2026-09-03 20:05:06 -0300

    feat: Exemplo de useState

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,8 +1,29 @@
 import imgQuadrado from "../../img/quadrado.png"
+import { useState } from "react";
+
+
 
 export default function Conteudo() {
+
+    let comum: number = 0;
+
+    function incrementaComum() {
+        comum++;
+        console.log("valor de comum: " + comum);
+    }
+    const[estado, setEstado] = useState<number>(0);
+    function incrementaEstado() {
+        setEstado(anterior => anterior + 1);
+        console.log("valor de estado: " + estado);
+    }
     return (
         <main>
+            <div>
+                <button onClick={incrementaComum}>Aumenta comum {comum}</button>
+            </div>
+            <div>
+                <button onClick={incrementaEstado}>Aumenta estado {estado}</button>
+            </div>
             <section>
                 <h2>Conteúdo</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat vero sed sapiente amet mollitia, praesentium veniam quisquam nesciunt eum repellendus est molestias fugit? Dolores iste voluptas rem quisquam excepturi quaerat ullam rerum quam voluptate dolore incidunt voluptatem impedit facere, totam id iusto nobis sit cupiditate quae autem, doloremque dignissimos. Quis.</p>        
