b07097add805653efa3bf1a50ae83357529bb2ef
Mateus-Isaque <mateusisaque2007@gmail.com>
2026-09-03 20:47:20 -0300

    feat: Construindo as rotas

---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,10 @@
+
+export default function EditarProduto() {
+  return (
+    <main>
+        <h2>
+            Editar Produtos
+        </h2>
+    </main>
+  )
+}
---
+++ my-app/src/routes/Error/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+
+
+export default function Error() {
+  return (
+    <main>
+        <h2>404 - Página não encontrada!</h2>
+        <div>
+            <Link to="/">Home</Link>
+        </div>
+    </main>
+  )
+}
