5ee4db8df39ca880b5c9c9f5eada291c76e8eba6
melissafiuza <melfiuza153@gmail.com>
2026-09-03 21:07:28 -0300

    feat: Rotas criadas.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,11 @@
 import Cabecalho from './components/Cabecalho'
-import Conteudo from './components/Conteudo'
 import Rodape from './components/Rodape'
-import Lampada from './components/Lampada'
+import { Outlet } from 'react-router'
 export default function App() {
   return (
     <>
       <Cabecalho />
-      <Conteudo />
-      <Lampada />
+      <Outlet />
       <Rodape />
     </>
   )
