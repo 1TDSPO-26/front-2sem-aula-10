f066faebd51598cfbb786d5776e31e53e161b957
Kauan Mattos <kauanosmattos@gmail.com>
2026-09-03 21:31:06 -0300

    feat: Implementando routes no main.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,12 @@
+import { Outlet } from "react-router";
 import Cabecalho from "./components/Cabecalho";
-import Conteudo from "./components/Conteudo";
-import Lampada from "./components/Lampada";
 import Rodape from "./components/Rodape";
 
 export default function App() {
   return (
     <div>
       <Cabecalho />
-      <Lampada />
-      <Conteudo />
+      <Outlet />
       <Rodape />
     </div>
   )
