91a03d7e0538e2637f6473094d0d0c382f97c43f
arthurmartinss <arthurmartinsh6@gmail.com>
2026-09-03 21:18:01 -0300

    feat: fazendo alteracoes importantes para rotas no projeto

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,12 @@
 import Cabecalho from "./components/Cabecalho";
-import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
-import Lampada from "./components/Lampada";
+import { Outlet } from "react-router";
 
 export default function App() {
   return (
     <div>
       <Cabecalho/>
-      <Lampada/>
-      <Conteudo/>
+      <Outlet/>
       <Rodape/>
       
     </div>
